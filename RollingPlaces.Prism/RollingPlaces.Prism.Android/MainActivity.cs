26515581d9725e3e94f98782c5ba721ac16cfc4f
JULIAN ANDRES OROZCO GARCIA <julianorozco236333@correo.itm.edu.co>
2020-05-09 12:08:39 -0500

    add colors and styles to the entire application

--- RollingPlaces.Prism/RollingPlaces.Prism.Android/MainActivity.cs
+++ RollingPlaces.Prism/RollingPlaces.Prism.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Syncfusion.SfRating.XForms.Droid;
 
 namespace RollingPlaces.Prism.Droid
 {
-    [Activity(Label = "RollingPlaces.Prism", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "RollingPlaces", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
