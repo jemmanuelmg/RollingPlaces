01526c6b79c18a80a4091948836d47329b6025d5
jemma <jemmanuelmg@hotmail.com>
2020-05-09 14:09:28 -0500

    added IApiService and ApiService to Common project

---
+++ RollingPlaces.Common/Models/TokenRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RollingPlaces.Common.Models
+{
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+
+}
---
+++ RollingPlaces.Common/Services/IApiService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using RollingPlaces.Common.Models;
+
+namespace RollingPlaces.Common.Services
+{
+    public interface IApiService
+    {
+        Task<Response> GetTokenAsync(string urlBase, string servicePrefix, string controller, TokenRequest request);
+
+        Task<Response> GetUserByEmail(string urlBase, string servicePrefix, string controller, string tokenType, string accessToken, EmailRequest request);
+
+        Task<bool> CheckConnectionAsync(string url);
+
+        Task<Response> RegisterUserAsync(string urlBase, string servicePrefix, string controller, UserRequest userRequest);
+
+        Task<Response> RecoverPasswordAsync(string urlBase, string servicePrefix, string controller, EmailRequest emailRequest);
+
+        Task<Response> PutAsync<T>(string urlBase, string servicePrefix, string controller, T model, string tokenType, string accessToken);
+
+        Task<Response> ChangePasswordAsync(string urlBase, string servicePrefix, string controller, ChangePasswordRequest changePasswordRequest, string tokenType, string accessToken);
+
+    }
+
+}
